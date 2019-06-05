7ed304187160b3e1f4c9bc07920eaff33f9f4ac4
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-04 21:57:45 -0400

    Add license

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule SSD1306.MixProject do
       app: :ssd1306,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,5 +25,16 @@ defmodule SSD1306.MixProject do
     [
       {:circuits_i2c, "~> 0.1"}
     ]
+  end
+
+  defp description do
+    "SSD1306 I2C driver"
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/nerves-training/ssd1306"}
+    }
   end
 end
